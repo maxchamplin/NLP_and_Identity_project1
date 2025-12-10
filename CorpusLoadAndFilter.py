de74186e53c76738aebe766bc5309ad029601426
maxchamplin <maxc1102@gmail.com>
2025-12-10 13:22:45 +0100

    Update CorpusLoadAndFilter.py

--- CorpusLoadAndFilter.py
+++ CorpusLoadAndFilter.py
@@ -12,7 +12,7 @@ def load_and_filter_corpus(path=str, desired_posts=None):
         text = 'No filtering applied to corpus.'
     
     if desired_posts is not None:
-        filtered_corpus = corpus.get_utterance_dataframe() #pull off top 1000 posts
+        filtered_corpus = corpus.get_utterances_dataframe() #pull off top 1000 posts
         #TODO make an option to get random posts, not first 1000
         text = f'Filtered corpus to first {desired_posts} posts.'
         
