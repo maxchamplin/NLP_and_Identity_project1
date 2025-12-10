f5c63a89bc8ec03d1e183e6462f48e3f91f4ae58
maxchamplin <maxc1102@gmail.com>
2025-12-10 12:06:12 +0100

    Update CorpusLoadAndFilter.py

--- CorpusLoadAndFilter.py
+++ CorpusLoadAndFilter.py
@@ -3,4 +3,6 @@ import lftk
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+def load_and_filter_corpus(path=str, desired_posts=None):
+    #get corpus loaded, and check if it needs to be cut filtered to desired post size.
+    corpus = convokit.Corpus(filename=path)
