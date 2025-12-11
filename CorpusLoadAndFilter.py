a226ed4a1a76c61207d93d408fe0cfe980044570
maxchamplin <maxc1102@gmail.com>
2025-12-11 12:44:34 +0100

    fighting with imports

--- CorpusLoadAndFilter.py
+++ CorpusLoadAndFilter.py
@@ -1,11 +1,13 @@
 from convokit import Corpus
-from convokit.text_processing import TextParser
+import regex
+from convokit import Corpus, download, TextParser, PolitenessStrategies, Classifier, BoWTransformer
+import random
 import random
 import nltk
 nltk.download('punkt_tab')
 import matplotlib.pyplot as plt
 import numpy as np
-
+import lftk
 def load_and_filter_corpus(path=str, desired_posts=None):
     #get corpus loaded, and check if it needs to be cut filtered to desired post size.
     corpus = convokit.Corpus(filename=path)
