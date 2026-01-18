fd4dc01a972ddf613f3f6f70d83aedc2186c358c
maxchamplin <maxc1102@gmail.com>
2026-01-18 14:23:52 +0100

    kind of gave up on having it be easy to reproduce

--- CorpusLoadAndFilter.py
+++ CorpusLoadAndFilter.py
@@ -17,27 +17,48 @@ return_format="list_dict")
 pos_features = [f['key'] for f in pos_features]
 additional_features = ["a_word_ps", "a_bry_ps", "corr_ttr"]
 features_to_extract = pos_features + additional_features
+
+def filter_corpus(corpus):
     
+        pre_filtered_corpus = corpus
+        
+        tokened = tokenize(pre_filtered_corpus)
+        excluded_utterances = []
+       
+        for utt in tokened.iter_utterances():
+            num_tokens = utt.meta.get('num_tokens', 0)
+            if num_tokens < 10 or num_tokens > 350:
+                excluded_utterances.append(utt.id)
+        print(excluded_utterances)
+        print(pre_filtered_corpus.get_utterance_ids())
+        
+        
     
+def tokenize(corpus):
+    def countTokens(text):
+        return len(text.split())
+
+    numtokens = TextProcessor(proc_fn=countTokens, output_field='num_tokens')
+    tokenized_corpus = numtokens.transform(corpus)
+    return tokenized_corpus
     
-def load_and_filter_corpus(path:str, desired_posts:None):
+def load_corpus(path:str, desired_posts:None):
     
     
     #TODO add in the filtering for the number of desired posts
     parser = TextParser(input_field='clean_text', verbosity=50,mode='tokenize')
 
     if desired_posts != None:
-        prefilter_num = desired_posts*2
-        pre_filtered_corpus = Corpus(filename=path, utterance_start_index=0, utterance_end_index=prefilter_num)
-        tokened = tokenize(pre_filtered_corpus)
+        
         
         filtered_corpus = Corpus(filename=path, utterance_start_index=0, utterance_end_index=desired_posts)
-    
+        
+       
     else: 
         
-        
+        print('no filtering applied')
         filtered_corpus = Corpus(filename=path)
-    print('Corpus loaded and tokenized.')
+    
         
         
     return  filtered_corpus
@@ -176,15 +197,67 @@ def visualize_differences(corpus1 ,corpus2, feature:str,corpus1_name:str,corpus2
     plt.title(f'Violin Plot of {feature}  for {corpus1_name} (mean: {corpus1_mean}) and {corpus2_name} (mean: {corpus2_mean})')
     plt.show()
     
+
+def analyze_differences_dataframe(corpus1, corpus2):
+    features = list(corpus1.columns)
+    for feature in features:
+        
+        
+        if 'meta.n' in feature or 'meta.f' in feature:
+
+            test_feature = []
+            
+            test_feature.append(list(corpus1[feature]))
+
+            for x in range(len(test_feature)):
+                Corpus1_median = test_feature[x]
+                Corpus1_median = [float(val) for val in Corpus1_median]
+                
+                
+            Corpus1_median_val = statistics.median(Corpus1_median)
+
+            test_feature2 = []
+            test_feature2.append(list(corpus2[feature]))
+
+            for x in range(len(test_feature2)):
+                Corpus2_median = test_feature2[x]
+                Corpus2_median = [float(val) for val in Corpus2_median]
+                
+                
+            Corpus2_median_val = statistics.median(Corpus2_median)
+            
+            
+            if Corpus1_median_val == 0  or Corpus2_median_val == 0 or Corpus1_median_val == 'nan'  or Corpus2_median_val == 'nan' :
+                pass
+            else:
+                try:
+                    print('___________','\n',f'item is {feature}','\n','Corpus 1 median value is:',Corpus1_median_val,'\n','Corpus 2 median is:',Corpus2_median_val,'\n','ratio between them is:',Corpus1_median_val/Corpus2_median_val)
+                except:
+                    print('###########','\n',f'attempted to divide by 0 for: {feature} median')
+        else:
+            pass
+
+def visualize_differences_dataframe(corpus1 ,corpus2, feature:str,corpus1_name:str,corpus2_name:str):
+    features = list(corpus1.columns)
+    if feature in features: 
+        pass
+    else:
+        raise Exception("input feature not in data")
     
-def tokenize(corpus):
-    def countTokens(text):
-        return len(text.split())
+            
+    corpus1_values = corpus1[feature].astype(float)
+    
+            
+    corpus2_values = corpus2[feature].astype(float)
 
-    numtokens = TextProcessor(proc_fn=countTokens, output_field='num_tokens')
-    tokenized_corpus = numtokens.transform(corpus)
-    return tokenized_corpus
-
+    corpus1_mean = statistics.mean(corpus1_values)
+    corpus2_mean = statistics.mean(corpus2_values)
+    plt.figure(figsize=(8,6))
+    plt.violinplot([corpus1_values,corpus2_values],showmeans=True)
+    plt.xticks([1, 2], [corpus1_name, corpus2_name])
+    plt.ylabel(feature)
+    plt.title(f'Violin Plot of {feature}  for {corpus1_name} (mean: {corpus1_mean}) and {corpus2_name} (mean: {corpus2_mean})')
+    plt.show()
 
 
 print('Corpus ready to be analyszed, file loaded correctly')
